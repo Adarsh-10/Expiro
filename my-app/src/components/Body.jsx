46cda8854e667cf834d5851b44183f9374ea69ec
Adarsh Sivasubramani <adarsh05100@gmail.com>
2022-08-27 20:40:12 -0400

    add create new item

--- my-app/src/components/Body.jsx
+++ my-app/src/components/Body.jsx
@@ -1,7 +1,45 @@
+import React from "react"
+import {nanoid} from "nanoid"
+
+
 export default function Body() {
+
+    console.log(localStorage.getItem("items"));
+
+    const [items, setItems] = React.useState(
+        JSON.parse(localStorage.getItem("items")) || []
+    )
+
+    React.useEffect(() => {
+        localStorage.setItem("items", JSON.stringify(items))
+    }, [items])
+
+    // function createNewItem(dateAdded, dateExpired, name, barcode) {
+    //     const newItem = {
+    //       id: nanoid(),
+    //       dateAdded,
+    //       dateExpired,
+    //       name,
+    //       barcode,
+    //     }
+    //     setItems(prevItems => [newItem, ...prevItems])
+    // }
+
+    function createNewItem() {
+        const newItem = {
+          id: nanoid(),
+          dateAdded: 0,
+          dateExpired: 0,
+          name: 0,
+          barcode: 0
+        }
+        setItems(prevItems => [newItem, ...prevItems])
+    }
+
     return (
         <div>
-            Body 
+            <button onClick={createNewItem}>Click Me!</button>
+            {items.dateAdded}
         </div>
     );
 }
